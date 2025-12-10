e32adc5f96f1284a6b68ab956485a51365cc4a86
Yunxiuuu <yunxiu327@gmail.com>
2025-12-10 16:22:52 +0800

    Create api.js for Axios instance configuration

---
+++ api.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const API_BASE = process.env.REACT_APP_API_BASE || 'http://localhost:4000';
+
+export default axios.create({
+  baseURL: API_BASE,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
