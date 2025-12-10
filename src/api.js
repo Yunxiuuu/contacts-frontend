75e029a661b5ae7ffb4964f1015e169359707640
Yunxiuuu <yunxiu327@gmail.com>
2025-12-10 17:04:46 +0800

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,9 +2,11 @@ import axios from 'axios';
 
 const API_BASE = process.env.REACT_APP_API_BASE || 'http://localhost:4000';
 
-export default axios.create({
+const instance = axios.create({
   baseURL: API_BASE,
   headers: {
     'Content-Type': 'application/json'
   }
 });
+
+export default instance;
